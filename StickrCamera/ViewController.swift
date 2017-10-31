81e4a8c681be8203836d7dcfab1a9b90fc9f7b2c
mrformidable <MichaelAi@michaels-macbook-pro-3.local>
2017-10-30 20:27:57 -0400

    Initial Commit

---
+++ StickrCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StickrCamera
+//
+//  Created by Michael A on 2017-10-30.
+//  Copyright © 2017 AI Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
